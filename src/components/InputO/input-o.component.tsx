70485c93f7480ca6e71f3badfa0e138c3634e9c6
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-23 07:56:21 +0530

    Add InputX, and InputO component

---
+++ src/components/InputO/input-o.component.tsx
@@ -0,0 +1,7 @@
+export default function InputO() {
+    return (
+        <div className="relative w-20 h-20">
+          <div className="absolute top-1/2 left-1/2 w-16 h-16 border-4 rounded-full border-black  transform -translate-y-1/2 -translate-x-1/2"></div>
+        </div>
+      );
+}
---
+++ src/components/InputX/input-x.component.tsx
@@ -0,0 +1,8 @@
+export default function InputX() {
+  return (
+    <div className="relative w-20 h-20">
+      <div className="absolute top-1/2 left-1/2 inset-0 w-full h-1 bg-black transform -translate-y-1/2 -translate-x-1/2 rotate-45"></div>
+      <div className="absolute top-1/2 left-1/2 inset-0 w-full h-1 bg-black transform -translate-y-1/2 -translate-x-1/2 -rotate-45"></div>
+    </div>
+  );
+}
